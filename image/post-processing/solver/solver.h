8e59db027ccdca24704eeda6f488bbaac187362d
mathis.villemin <mathis.villemin@epita.fr>
2022-10-30 16:55:21 +0100

    push this fucking file

---
+++ image/post-processing/solver/solver.h
@@ -0,0 +1,18 @@
+#ifndef SOLVER_H
+#define SOLVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <err.h>
+#endif
+#define LENGTH 3
+
+
+void check(int grid[][LENGTH*LENGTH],int res[], size_t line, size_t column);
+// int showgrid(int grid[][LENGTH*LENGTH]);
+int is_solve(int grid[][LENGTH*LENGTH]);
+int getsudoku(char* filename, int sudoku[][LENGTH*LENGTH]);
+int solver(int grid[][LENGTH*LENGTH]);
+
