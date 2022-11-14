9351f406570a87bd019da0565ad69c76c492d3ca
Mathis Villemin <mathis.villemin@epita.fr>
2022-11-14 13:54:37 +0100

    fonctionne

--- picture-processing/post-processing/reconstruction-grid/reconstruction_grid.c
+++ picture-processing/post-processing/reconstruction-grid/reconstruction_grid.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <err.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#define LENGTH = 3
+
+SDL_Surface* load_image(const char* path)
+{
+    // Load the image into a temporary surface.
+    SDL_Surface* tempSurface = IMG_Load(path);
+    if (tempSurface == NULL) errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // Create a new surface in the desired format.
+    SDL_Surface* surface = SDL_ConvertSurfaceFormat(tempSurface,
+            SDL_PIXELFORMAT_RGB888, 0);
+    if (surface == NULL) errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // Free the temporary surface.
+    SDL_FreeSurface(tempSurface);
+
+    // Return the surface in the desired format.
+    return surface;
+}
+
+
 int main(int argc, char** argv)
 {
     // Checks the number of arguments.
@@ -10,15 +30,20 @@ int main(int argc, char** argv)
     arg1 : image's name
     */
     if (argc != 2)
-        errx(EXIT_FAILURE, "need 1 argument\n
-        image's name\n");
+    {
+        errx(EXIT_FAILURE, "need 1 argument\n"
+        "image's name\n");
+    }
 
      // Initializes the SDL.
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-
+    }
+    int LENGTH = 3;
+    char casename[] = {'S','u','d','o','k','u','(','0',',','0',')',0};
     // Create a surface from the colored image.
-    SDL_Surface* sudokuCase = load_image("SudokuCase/%s(0,0).jpeg",argv[1]);//tester l'existance
+    SDL_Surface* sudokuCase = load_image(casename);//tester l'existance
     SDL_Surface* sudokuGrid = SDL_CreateRGBSurface(0,sudokuCase->w * LENGTH * LENGTH,sudokuCase->h * LENGTH * LENGTH,32,0,0,0,0); //creat a black image in function of the width and height of sudokucase
 
 
@@ -30,13 +55,15 @@ int main(int argc, char** argv)
         spriteDst.x = 0;
         for (int col = 0; col < LENGTH*LENGTH; col++)
         {
-            SDL_Surface* sudokuCase = load_image("SudokuCase/%s(%i,%i).jpeg",argv[1],row,col);
+            casename[7] =  '0' + row;
+            casename[9] =  '0' + col;
+            SDL_Surface* sudokuCase = load_image(casename);
             SDL_BlitSurface(sudokuCase, NULL, sudokuGrid, &spriteDst);
-            spriteDst.x += sudokuCase->h;//define the position of each case in sudokuGrid
+            spriteDst.x += sudokuCase->w;//define the position of each case in sudokuGrid
         }
-        spriteDst.y += sudokuCase->w; //define the position of each case in sudokuGrid
+        spriteDst.y += sudokuCase->h; //define the position of each case in sudokuGrid
     }
-    if (IMG_SaveJPG(sudokuGrid,  "SudokuCase.jpeg.result",100); != 0) errx(EXIT_FAILURE, "%s", SDL_GetError());
+    if (IMG_SaveJPG(sudokuGrid, "resultname.result",100) != 0) errx(EXIT_FAILURE, "%s", SDL_GetError());
 
 
 
