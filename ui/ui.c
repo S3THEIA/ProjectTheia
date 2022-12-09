4d0ec8841a2df290d0cf83759bb56180c087cd7d
augustin.glorian <augustin.glorian@epita.fr>
2022-12-09 12:38:58 +0100

    Add minim ui
    Signed-off-by: augustin.glorian <augustin.glorian@epita.fr>

---
+++ ui/ui.c
@@ -0,0 +1,31 @@
+#include <gtk/gtk.h>
+
+// Main function.
+int main (int argc, char *argv[])
+{
+    // Initializes GTK.
+    gtk_init(NULL, NULL);
+
+    // Loads the UI description and builds the UI.
+    // (Exits if an error occurs.)
+    GtkBuilder* builder = gtk_builder_new();
+    GError* error = NULL;
+    if (gtk_builder_add_from_file(builder, "ui.glade", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    // Gets the widgets.
+    GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.duel"));
+
+    // Connects signal handlers.
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Runs the main loop.
+    gtk_main();
+
+    // Exits.
+    return 0;
+}
