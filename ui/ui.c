68d62aba0418ae063af21c91a752c577b9a2552b
augustin.glorian <augustin.glorian@epita.fr>
2022-12-09 23:07:16 +0100

    Add boutons: load-image, quit
    Signed-off-by: augustin.glorian <augustin.glorian@epita.fr>

--- ui/ui.c
+++ ui/ui.c
@@ -1,13 +1,15 @@
 #include <gtk/gtk.h>
 
+// Variables
+GtkWidget* window;
+GtkWidget* image;
+
 // Main function.
 int main (int argc, char *argv[])
 {
     // Initializes GTK.
     gtk_init(NULL, NULL);
 
-    // Loads the UI description and builds the UI.
-    // (Exits if an error occurs.)
     GtkBuilder* builder = gtk_builder_new();
     GError* error = NULL;
     if (gtk_builder_add_from_file(builder, "ui.glade", &error) == 0)
@@ -17,15 +19,49 @@ int main (int argc, char *argv[])
         return 1;
     }
 
+    gtk_builder_connect_signals(builder, NULL);
+
+
     // Gets the widgets.
-    GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "win.ui"));
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main-window"));
+    image = GTK_WIDGET(gtk_builder_get_object(builder, "main-picture"));
+
 
     // Connects signal handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
 
     // Runs the main loop.
     gtk_main();
 
     // Exits.
     return 0;
+}
+
+
+// Function with button_quit
+void quit_app(GtkButton* b)
+{
+    gtk_main_quit();
+}
+
+// Function with button_load-image
+void click_load_image(GtkButton* b)
+{
+    GtkWidget* dialog;
+    gint result;
+    char* filename;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+    dialog = gtk_file_chooser_dialog_new("Open File", GTK_WINDOW(window),
+            action, ("Cancel"), GTK_RESPONSE_CANCEL, ("Open"),
+            GTK_RESPONSE_ACCEPT, NULL);
+    result = gtk_dialog_run(GTK_DIALOG (dialog));
+    if (result == GTK_RESPONSE_ACCEPT) {
+        GtkFileChooser* chooser = GTK_FILE_CHOOSER(dialog);
+        filename = gtk_file_chooser_get_filename(chooser);
+    }
+
+    gtk_widget_destroy(dialog);
+    if (result == GTK_RESPONSE_ACCEPT)
+        gtk_image_set_from_file(GTK_IMAGE(image), filename);
 }
